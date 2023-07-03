a9d64dc42d90a492e81f2ab5ff2ebe81b3843e1a
arseny <76913432+arseny-sh@users.noreply.github.com>
2023-07-03 14:44:48 +0300

    Update 1.1.0

--- include/gd/layers_scenes_transitions_nodes/PauseLayer.h
+++ include/gd/layers_scenes_transitions_nodes/PauseLayer.h
@@ -12,8 +12,23 @@
 		bool m_bUnknown2;
 
 	public:
-		void createToggleButton(cocos2d::SEL_MenuHandler callback, bool on,
-			cocos2d::CCMenu* menu, std::string caption, cocos2d::CCPoint pos) ;
+		// Platinum SDK 1.1.0 Update
+		virtual bool init(bool options)
+		virtual void keyBackClicked()
+		virtual void keyDown(cocos2d::enumKeyCodes key)
+		void createToggleButton(cocos2d::SEL_MenuHandler callback, bool on, cocos2d::CCMenu* menu, std::string caption, cocos2d::CCPoint pos);
+   		void onAutoRetry(cocos2d::CCObject* pSender pSender)
+		void onNormalMode(cocos2d::CCObject* pSender)
+		void onProgressBar(cocos2d::CCObject* pSender)
+		void onPracticeMode(cocos2d::CCObject* pSender)
+		void onAutoCheckpoints(cocos2d::CCObject* pSender)
+		void musicSliderChanged(cocos2d::CCObject* pSender)
+		void onEdit(cocos2d::CCObject* pSender)
+		void onHelp(cocos2d::CCObject* pSender)
+		void onQuit(cocos2d::CCObject* pSender)
+		void onTime(cocos2d::CCObject* pSender)
+		void onResume(cocos2d::CCObject* pSender)
+		void onRestart(cocos2d::CCObject* pSender)
 	};
 
 
